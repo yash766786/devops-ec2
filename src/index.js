26296ca2843a57622ce9f450b865c615de7dceeb
yash766786 <yashchoudhary766786@gmail.com>
2026-07-05 20:51:52 +0530

    writing CD workflow

--- src/index.js
+++ src/index.js
@@ -13,6 +13,14 @@ app.get('/health', (req, res) => {
   res.status(200).send('OK!');
 });
 
+app.get('/data', (req, res) => {
+  const data = {
+    message: 'This is some sample data from the API.',
+    timestamp: new Date()
+  };
+  res.json(data);
+});
+
 app.listen(PORT, () => {
   console.log(`PORT: ${process.env.PORT}`);
   console.log(`Server is running on port ${PORT}`);
